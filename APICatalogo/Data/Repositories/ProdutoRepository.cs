e0d8fb95995c8fab6484e63a51ab8d8ac3d517bf
Lucas <lucasgunner3@gmail.com>
2021-10-18 23:51:28 -0300

    Aplicando padrão repository

---
+++ APICatalogo/Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApiCatalogo.Repositories;
+using APICatalogo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ApiCatalogo.Data.Repositories
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Produto> GetProdutosPorPreco()
+        {
+            return Get().ToList().OrderBy(x => x.Preco);
+        }
+    }
+}
---
+++ APICatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using APICatalogo.Models;
+using System.Collections.Generic;
+
+namespace ApiCatalogo.Repositories
+{
+    public interface ICategoriaRepository : IRepository<Categoria>
+    {
+        IEnumerable<Categoria> GetCategoriasProdutos();
+    }
+}
